563b29fe4b20f24bb1452bcd0ffadf0f843c2c1e
BOOK-QEH0KSANTE\김경우 <kku898989@naver.com>
2026-03-20 20:29:07 +0900

    3주차 과제: 진수 변환 프로그램

---
+++ radix_convert.py
@@ -0,0 +1,12 @@
+base = int(input("입력 진수 결정(16/10/8/2): "))
+num_str = input("값 입력: ")
+
+if base not in [16, 10, 8, 2]:
+    print("지원하지 않는 진수입니다.")
+else:
+    num = int(num_str, base)
+
+    print("16진수 ==> ", hex(num))
+    print("10진수 ==> ", num)
+    print("8진수 ==> ", oct(num))
+    print("2진수 ==> ", bin(num))
